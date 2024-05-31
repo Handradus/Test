13a3404ab26285c1ca23bef8d031caa6f7b8ce7f
Handrades <humberto.andrades2201@alumnos.ubiobio.cl>
2024-05-31 17:41:54 -0400

    modifico y agrego tipos de datos al js

--- jstest.js
+++ jstest.js
@@ -1,2 +1,32 @@
 let edad = 30;
 const nombre= "Humberto Andrades";
+console.log(nombre,edad);
+
+let datos ={
+    nombre,
+    edad,
+    pais:"Chile",
+
+}
+
+console.log(datos.nombre,datos.pais);
+
+//Ojo un array en con []
+
+let numeros = [1,2,3,4,5,6];
+console.log (numeros);
+
+console.log (typeof numeros);
+
+function sumar(a, b) {
+    return a + b;
+  }
+
+
+  let res =sumar (numeros[2],numeros[4]);
+
+  console.log (res)
+
+  let fechaActual = new Date();
+
+  console.log (fechaActual);
