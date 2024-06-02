c4fe6bcc3469f30280ab2eade9f846da82e0c4dc
Handrades <humberto.andrades2201@alumnos.ubiobio.cl>
2024-06-02 18:27:44 -0400

    Archivo con codigo referente al uso de strings

---
+++ strings.js
@@ -0,0 +1,5 @@
+const primeraParte = 'Hola'
+const segundaParte = 'Soy'
+const terceraParte = 'Diego Salazar Jara'
+const presentacion = [primeraParte, segundaParte, terceraParte].join("-")
+console.log(presentacion) //Hola Soy Diego Salazar Jara
